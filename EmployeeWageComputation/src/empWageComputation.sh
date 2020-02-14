f6bd299836975ac9d433f62b9f19454432dbbea3
kishorereddy0909 <kishorereddy996@gmail.com>
2020-02-14 16:40:10 +0530

    [kishorereddy] Refactor. added employeedailywages for full time in empWageComputation.sh in src

--- EmployeeWageComputation/src/empWageComputation.sh
+++ EmployeeWageComputation/src/empWageComputation.sh
@@ -5,3 +5,8 @@ if [ $random -eq 1 ];
  then
    echo "Employee is present "
 fi
+fullTimeDailyWage=0;
+wagePerHour=20;
+hoursForADayFullTime=8;
+fullTimeDailyWage=$(( $wagePerHour*$hoursForADayFullTime ));
+echo $fullTimeDailyWage
